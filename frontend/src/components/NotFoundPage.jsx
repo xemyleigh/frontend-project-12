0e40b2de19d0e6685e13710c7b15458d67e8c940
alexander <emyleigh@inbox.ru>
2022-10-09 03:19:10 +0300

    create 404 page

---
+++ frontend/src/components/NotFoundPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFoundPage = () => {
+    return (
+        <div className='container-fliud'>
+            <div className='row align-content-center justify-content-center'>
+                <div className='col-6'>
+                    <h1>404 PAGE NOT FOUND</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NotFoundPage
