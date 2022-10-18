63ecd9007bd12b580f2b11a8b7be80e00923b3b8
alexander <emyleigh@inbox.ru>
2022-10-18 18:53:30 +0300

    add slice for modals

---
+++ frontend/src/slices/modalSlice.js
@@ -0,0 +1,22 @@
+import { createSlice, current } from '@reduxjs/toolkit'
+
+const modalSlice = createSlice({
+    name: 'modalInfo',
+    initialState: { modalType: null, channelId: null },
+    reducers: {
+        openModal(state, { payload }) {
+            const { modalType, id } = payload
+            console.log(payload)
+            state.modalType = modalType
+            state.channelId = id
+            console.log(current(state))
+        },
+
+        closeModal(state, actions) {
+            state.modalType = null
+        }
+    }
+})
+
+export const { actions } = modalSlice
+export default modalSlice.reducer
