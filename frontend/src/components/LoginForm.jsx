b0fd7f1b87b04eaef763b70bac434ee040256082
alexander <emyleigh@inbox.ru>
2022-10-26 15:26:52 +0300

    add useEffect hook import

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useFormik } from 'formik';
 import Form from '../../node_modules/react-bootstrap/Form'
 import Button from '../../node_modules/react-bootstrap/Button'
